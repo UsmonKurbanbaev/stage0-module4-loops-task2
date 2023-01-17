73bcd81af6c30f3da397c22942599e1e1c889db7
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-01-17 21:21:32 +0500

    for loops task2

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,13 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int size = chars.length;
-        System.out.println(chars);
-    }
-
-    public static void main(String[] args) {
-        WordsBuilder builder = new WordsBuilder();
-
-        builder.buildPhrase('l', 'o', 'o', 'p', 's', ' ', 'a', 'r', 'e', ' ', 'c', 'o', 'o', 'l');
+        int len = chars.length, index = 0;
+        while(index < len) {
+            System.out.print(chars[index++]);
+        }
     }
 }
